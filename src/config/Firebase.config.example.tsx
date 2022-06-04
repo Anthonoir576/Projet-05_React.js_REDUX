082b3d2dedc261a092e80b96e4088088668cefb2
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-04 13:53:43 +0200

    :FEAT: => Connection a firebase

---
+++ src/config/Firebase.config.example.tsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig :any = {
+  apiKey             : "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain         : "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId          : "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket      : "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId  : "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  appId              : "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
+
+
+const app :any = initializeApp(firebaseConfig);
