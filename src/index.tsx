43211103999c184f23944d20c83828ded077211f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-06-04 11:06:26 +0200

    :BUILD: => build app

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React    from 'react';
 import ReactDOM from 'react-dom/client';
 import App      from './App';
 
+import './styles/style.css';
+
+
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,5 +16,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
